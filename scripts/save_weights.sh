51f6fe861f0db04412312a095854224a90354ecc
octavmatu <florinoctavian.matu@gmail.com>
2021-03-05 17:57:02 +0200

    Update save_weights.sh

--- scripts/save_weights.sh
+++ scripts/save_weights.sh
@@ -6,7 +6,9 @@ UNIX_EPOCH=$(date +%s)
 # Get basename of the network file, since Darknet saves the weights as 'NetworkFileBaseName_final.weights'
 NETWORK_FILENAME=${NETWORK_FILENAME##*/}
 BASE_NAME=${NETWORK_FILENAME%.cfg}
+OUTPUT_PATH = "s3://${S3_BUCKET_NAME}/trained/${UNIX_EPOCH}"
+echo "Uploading final weights, ${BASE_NAME}_${UNIX_EPOCH}.weights, and config file, ${NETWORK}, to ${OUTPUT_PATH}"
 
-echo "Uploading final weights to s3://${S3_BUCKET_NAME}/models/${BASE_NAME}_${UNIX_EPOCH}.weights"
-
-aws s3 cp backup/${BASE_NAME}_final.weights s3://${S3_BUCKET_NAME}/models/${BASE_NAME}_${UNIX_EPOCH}.weights
+aws s3 cp ${NETWORK} ${OUTPUT_PATH}/${NETWORK}
+aws s3 cp backup/${BASE_NAME}_final.weights ${OUTPUT_PATH}/${BASE_NAME}_${UNIX_EPOCH}_final.weights
+aws s3 cp backup/${BASE_NAME}_best.weights ${OUTPUT_PATH}/${BASE_NAME}_${UNIX_EPOCH}_best.weights
