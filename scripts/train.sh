e6358fa8f75bab51c62f9a56c7bc0dd574cd2cf8
octavmatu <florinoctavian.matu@gmail.com>
2021-03-08 19:16:51 +0200

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -12,19 +12,19 @@ aws s3 sync s3://${S3_BUCKET_NAME}/data data/
 aws s3 sync s3://${S3_BUCKET_NAME}/pretrained pretrained/
 
 if [ -f "cfg/${NETWORK_FILENAME}" ]; then
-    export NETWORK ="cfg/${NETWORK_FILENAME}"
+    export OCTAV_NETWORK="cfg/${NETWORK_FILENAME}"
 fi
 
 if [ -f "pretrained/${PRETRAINED_WEIGHTS_FILENAME}" ]; then
-    export PRETRAINED_WEIGHTS="pretrained/${PRETRAINED_WEIGHTS_FILENAME}"
+    export OCTAV_PRETRAINED_WEIGHTS="pretrained/${PRETRAINED_WEIGHTS_FILENAME}"
 fi
 
 if [ -f "data/${DATA_FILENAME}" ]; then
-    export DATA="data/${DATA_FILENAME}"
+    export OCTAV_DATA="data/${DATA_FILENAME}"
 fi
 
 echo "Start training at $(date +"%D %T")"
-./darknet/darknet detector train ${DATA} ${NETWORK} ${PRETRAINED_WEIGHTS} -dont_show -mjpeg_port 8090 -mjson_port 9070 -map 
+./darknet/darknet detector train ${OCTAV_DATA} ${OCTAV_NETWORK} ${OCTAV_PRETRAINED_WEIGHTS} -dont_show -mjpeg_port 8090 -mjson_port 9070 -map 
 
 echo "Finished training at $(date +"%D %T")"
 
