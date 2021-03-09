91b6eac24c2321b85cd2071db34dfdd969e34ad8
octavmatu <florinoctavian.matu@gmail.com>
2021-03-09 15:36:42 +0200

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,16 +6,18 @@ echo "Getting data from S3"
 if [ ! -d "backup" ]; then
     mkdir backup
 fi
-  
-aws s3 sync s3://${S3_BUCKET_NAME}/cfg cfg/
-aws s3 sync s3://${S3_BUCKET_NAME}/data/images data/images 
-#aws s3 sync s3://${S3_BUCKET_NAME}/data/labels data/images 
-aws s3 sync s3://${S3_BUCKET_NAME}/data/labels data/labels 
-aws s3 sync s3://${S3_BUCKET_NAME}/data/obj.data data/obj.data 
-aws s3 sync s3://${S3_BUCKET_NAME}/data/obj.names data/obj.names
-aws s3 sync s3://${S3_BUCKET_NAME}/data/test.txt data/train.txt 
-aws s3 sync s3://${S3_BUCKET_NAME}/data/train.txt data/train.txt  
-aws s3 sync s3://${S3_BUCKET_NAME}/pretrained pretrained/
+
+if [${DOWNLOAD_DATA} -gt 0]; then
+    aws s3 sync s3://${S3_BUCKET_NAME}/cfg cfg/
+    aws s3 sync s3://${S3_BUCKET_NAME}/data/images data/images 
+    #aws s3 sync s3://${S3_BUCKET_NAME}/data/labels data/images 
+    aws s3 sync s3://${S3_BUCKET_NAME}/data/labels data/labels 
+    aws s3 sync s3://${S3_BUCKET_NAME}/data/obj.data data/obj.data 
+    aws s3 sync s3://${S3_BUCKET_NAME}/data/obj.names data/obj.names
+    aws s3 sync s3://${S3_BUCKET_NAME}/data/test.txt data/train.txt 
+    aws s3 sync s3://${S3_BUCKET_NAME}/data/train.txt data/train.txt  
+    aws s3 sync s3://${S3_BUCKET_NAME}/pretrained pretrained/
+fi
 
 if [ -f "cfg/${NETWORK_FILENAME}" ]; then
     export OCTAV_NETWORK="cfg/${NETWORK_FILENAME}"
